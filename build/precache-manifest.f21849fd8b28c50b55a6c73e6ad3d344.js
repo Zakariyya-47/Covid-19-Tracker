1485a5038ef1b26e9600efaa7eb6b84a933a03de
Zakariyya-47 <65165714+Zakariyya-47@users.noreply.github.com>
2020-08-03 15:28:56 +0200

    Add files via upload

---
+++ build/precache-manifest.f21849fd8b28c50b55a6c73e6ad3d344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36b2b552aeae06a6129b85ba2729d86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c339f768e3c984fefd3",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "e2cb8a9bdc581828d8bd",
+    "url": "/static/css/main.12a3e855.chunk.css"
+  },
+  {
+    "revision": "6c339f768e3c984fefd3",
+    "url": "/static/js/2.4ea85b79.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.4ea85b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2cb8a9bdc581828d8bd",
+    "url": "/static/js/main.2ce85fbd.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
